2240691a1e9ec97078ce906185e9c40b2dd2142c
Pedro Cunha <53006840+pedro-acunha@users.noreply.github.com>
2022-04-13 13:16:42 +0100

    Update clf_multi.py

--- clf_multi.py
+++ clf_multi.py
@@ -24,6 +24,33 @@ plt.rcParams["figure.figsize"] = (20,20)
 plt.rcParams.update({'font.size': 22})
 import seaborn as sns
 
+def plot_feature_importance(importance,names,model_type):
+    """Plots feature importance
+    importance: features importance output from model
+    names: features names
+    model_type: name of the model used to compute features importance
+  """
+    
+    #Create arrays from feature importance and feature names
+    feature_importance = np.array(importance)
+    feature_names = np.array(names)
+    
+    #Create a DataFrame using a Dictionary
+    data={'feature_names':feature_names,'feature_importance':feature_importance}
+    fi_df = pd.DataFrame(data)
+    
+    #Sort the DataFrame in order decreasing feature importance
+    fi_df.sort_values(by=['feature_importance'], ascending=False,inplace=True)
+    
+    #Define size of bar plot
+    plt.figure(figsize=(20,20))
+    #Plot Searborn bar chart
+    sns.barplot(x=fi_df['feature_importance'], y=fi_df['feature_names'])
+    #Add chart labels
+    plt.title(model_type + ' Feature Importance')
+    plt.xlabel('Feature Importance')
+    plt.ylabel('Feature Names')
+    
 # Outlier detection
 
 def flag_cat_outliers(x, var1, var2):
